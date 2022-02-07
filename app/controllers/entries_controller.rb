fef3b3ffb0283e91c355d681c129c7e180b64ae9
Sam Ruby <rubys@intertwingly.net>
2022-02-07 14:35:07 -0500

    entry updates: heats

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -14,31 +14,27 @@ class EntriesController < ApplicationController
   def new
     @entry = Entry.new
 
-    @person = Person.find(params[:primary])
-    entries = @person.lead_entries + @person.follow_entries
-    studios = [@person.studio] + @person.studio.pairs
+    form_init
 
-    @dances = Dance.all.map(&:name)
-
-    seeking = @person.role == 'Leader' ? 'Follower' : 'Leader'
-    teacher = Person.where(type: 'Professional', studio: studios, 
-      role: [seeking, 'Both']).order(:name)
-    student = Person.where(type: 'Student', studio: @person.studio, 
-      role: [seeking, 'Both']).order(:name)
-
-    @avail = teacher + student
-    surname = @person.name.split(',').first + ','
-    spouse = @avail.find {|person| person.name.start_with? surname}
-    @avail = ([spouse] + @avail).uniq if spouse
-
-    @avail = @avail.map {|person| [person.display_name, person.name]}.to_h
-
-    @ages = Age.all.order(:id).map {|age| [age.description, age.id]}
-    @levels = Level.all.order(:id).map {|level| [level.name, level.id]}
+    @partner = nil
+    @age = @person.age_id
+    @level = @person.level_id
   end
 
   # GET /entries/1/edit
   def edit
+    form_init
+
+    if @person.role = "Follower"
+      @partner = @entry.lead.name
+    else
+      @partner = @entry.follow.name
+    end
+
+    @age = @entry.age_id
+    @level = @entry.level_id
+
+    tally_entry
   end
 
   # POST /entries or /entries.json
@@ -47,7 +43,7 @@ class EntriesController < ApplicationController
 
     @person = Person.find(entry[:primary])
 
-    if @person.role = "Follower"
+    if @person.role == "Follower"
       lead = Person.find_by(name: entry[:partner])
       follow = @person
     else
@@ -62,28 +58,11 @@ class EntriesController < ApplicationController
       level_id: entry[:level]
     )
 
-    total = 0
-    %w(Closed Open).each do |category|
-      Dance.all.each do |dance|
-        count = entry[:entries][category][dance.name].to_i
-        if count > 0
-          total += count
-
-          (count..1).each do |heat|
-            Heat.create({
-              number: 0, 
-              entry: @entry,
-              category: category,
-              dance: dance
-            })
-          end
-        end
-      end
-    end
+    update_heats(entry)
 
     respond_to do |format|
       if @entry.save
-        format.html { redirect_to @person, notice: "#{total} heats successfully created." }
+        format.html { redirect_to @person, notice: "#{@total} heats successfully created." }
         format.json { render :show, status: :created, location: @entry }
       else
         @primary = @person
@@ -95,9 +74,23 @@ class EntriesController < ApplicationController
 
   # PATCH/PUT /entries/1 or /entries/1.json
   def update
+    entry = params[:entry]
+
+    @person = Person.find(entry[:primary])
+
+    if @person.role == "Follower"
+      lead = Person.find_by(name: entry[:partner])
+      follow = @person
+    else
+      lead = @person
+      follow = Person.find_by(name: entry[:partner])
+    end
+
+    update_heats(entry)
+
     respond_to do |format|
       if @entry.update(entry_params)
-        format.html { redirect_to entry_url(@entry), notice: "Entry was successfully updated." }
+        format.html { redirect_to @person, notice: "#{@total} heats changed." }
         format.json { render :show, status: :ok, location: @entry }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -125,5 +118,67 @@ class EntriesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def entry_params
       params.require(:entry).permit(:count, :dance_id, :lead_id, :follow_id)
+    end
+
+    def form_init
+      @person = Person.find(params[:primary])
+      entries = @person.lead_entries + @person.follow_entries
+      studios = [@person.studio] + @person.studio.pairs
+  
+      @dances = Dance.all.map(&:name)
+  
+      seeking = @person.role == 'Leader' ? 'Follower' : 'Leader'
+      teacher = Person.where(type: 'Professional', studio: studios, 
+        role: [seeking, 'Both']).order(:name)
+      student = Person.where(type: 'Student', studio: @person.studio, 
+        role: [seeking, 'Both']).order(:name)
+  
+      @avail = teacher + student
+      surname = @person.name.split(',').first + ','
+      spouse = @avail.find {|person| person.name.start_with? surname}
+      @avail = ([spouse] + @avail).uniq if spouse
+  
+      @avail = @avail.map {|person| [person.display_name, person.name]}.to_h
+  
+      @ages = Age.all.order(:id).map {|age| [age.description, age.id]}
+      @levels = Level.all.order(:id).map {|level| [level.name, level.id]}
+
+      @entries = {'Closed' => {}, 'Open' => {}}
+    end
+
+    def tally_entry
+      @entries = {'Closed' => {}, 'Open' => {}}
+
+      @entries.merge!(@entry.heats.group_by {|heat| heat.category}.map do |category, heats|
+        [category, heats.group_by {|heat| heat.dance.name}]
+      end.to_h)
+    end
+
+    def update_heats(entry)
+      tally_entry
+
+      @total = 0
+      %w(Closed Open).each do |category|
+        Dance.all.each do |dance|
+          was = @entries[category][dance.name]&.length || 0
+          wants = entry[:entries][category][dance.name].to_i
+          if wants != was
+            @total += (wants - was).abs
+  
+            (wants...was).each do |heat|
+              @entries[category][dance.name][heat].destroy!
+            end
+  
+            (was...wants).each do |heat|
+              Heat.create({
+                number: 0, 
+                entry: @entry,
+                category: category,
+                dance: dance
+              })
+            end
+          end
+        end
+      end
     end
 end
