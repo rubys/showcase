26837dc880be1e7c95723242e01ad025d0c272a6
Sam Ruby <rubys@intertwingly.net>
2022-02-11 15:30:56 -0500

    add checknumber support

---
+++ app/javascript/controllers/check_number_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="check-number"
+export default class extends Controller {
+  connect() {
+    let checkboxes = this.element.querySelectorAll('input[type=checkbox]');
+    for (let checkbox of [...checkboxes]) {
+      checkbox.addEventListener("keydown", event => {
+        let input = document.createElement('input');
+        input.setAttribute('type', 'text');
+        input.setAttribute('id', checkbox.getAttribute('id'));
+        input.setAttribute('name', checkbox.getAttribute('name'));
+        input.classList.add('entry-count');
+        input.value = event.key;
+        checkbox.replaceWith(input);
+      });
+    }
+  }
+}
