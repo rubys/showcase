8e32e0d34c06ea527dbcd5cd19ef533f98495caf
Sam Ruby <rubys@intertwingly.net>
2022-02-19 19:47:37 -0500

    get event test passing

--- test/controllers/event_controller_test.rb
+++ test/controllers/event_controller_test.rb
@@ -2,7 +2,27 @@ require "test_helper"
 
 class EventControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
-    get event_root_url
+    get root_url
     assert_response :success
+  end
+
+  test "should get settings" do
+    get settings_event_index_path
+    assert_response :success
+  end
+
+  test "should update settings" do
+    @event = Event.last
+    patch event_url(@event), params: { event: {
+      name: @event.name,
+      location: @event.location,
+      date: @event.date,
+      heat_range_cat: @event.heat_range_cat,
+      heat_range_level: @event.heat_range_level,
+      heat_range_age: @event.heat_range_age,
+      intermix: @event.intermix
+    } }
+
+    assert_redirected_to settings_event_index_path
   end
 end
