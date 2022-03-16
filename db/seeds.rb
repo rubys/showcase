0c843c7367f7f99cd5373632744fbdd58dc64732
Sam Ruby <rubys@intertwingly.net>
2022-03-16 15:45:59 -0400

    add generic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 if ENV['SHOWCASE'] == 'Raleigh'
   # require_relative 'seeds/raleigh.rb'
-else
+elsif ENV['SHOWCASE'] == 'Harrisburg'
   require_relative 'seeds/harrisburg.rb'
+else
+  require_relative 'seeds/generic.rb'
 end
---
+++ db/seeds/generic.rb
@@ -0,0 +1,32 @@
+require 'yaml'
+
+config = YAML.load(IO.read "#{__dir__}/generic.yaml")
+
+###
+
+Event.delete_all
+Event.create!(
+  name: config[:settings][:event][:name],
+  location: config[:settings][:event][:location],
+  date: config[:settings][:event][:date],
+  heat_range_cat: config[:settings][:heat][:category],
+  heat_range_level: config[:settings][:heat][:level],
+  heat_range_age: config[:settings][:heat][:age],
+)
+
+Age.delete_all
+ages = config[:ages].map do |category, description|
+  [category, Age.create!(category: category, description: description)]
+end.to_h
+
+Level.delete_all
+levels = config[:levels].map do |name|
+  [name, Level.create!(name: name)]
+end.to_h
+
+Dance.delete_all
+order = 0
+config[:dances].to_a.map do |dance|
+  order += 1
+  [dance, Dance.create!(name: dance, order: order)]
+end.to_h
