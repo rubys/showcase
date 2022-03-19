da8c125949a26fd6f54ed1239275f0725f43342e
Sam Ruby <rubys@intertwingly.net>
2022-03-19 16:07:47 -0400

    that didn't work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,10 +33,6 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
-  if ENV['RAILS_APP_SCOPE'].present?
-    config.assets.prefix = "/#{ENV['RAILS_APP_SCOPE']}/assets"
-  end
-
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
