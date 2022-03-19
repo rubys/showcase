988b8766a6d4ca0216dea6821155c60acc5818e9
Sam Ruby <rubys@intertwingly.net>
2022-03-19 15:30:07 -0400

    scope assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,10 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
+  if ENV['RAILS_APP_SCOPE']
+    config.assets.prefix = "/#{ENV['RAILS_APP_SCOPE']}/assets""
+  end
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
