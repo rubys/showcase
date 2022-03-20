4f77bc275bdffa5a370603236976b42ecfb9fa57
Sam Ruby <rubys@intertwingly.net>
2022-03-20 17:20:04 -0400

    try what the docs suggest

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module AmEvent
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.action_cable.mount_path = '/websocket'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
