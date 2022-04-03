d96db21aeb985d360f71596e95205737528abc9f
Sam Ruby <rubys@intertwingly.net>
2022-04-03 11:43:22 -0400

    dynamic web socket

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,4 @@ class ApplicationController < ActionController::Base
     redirect_back fallback_location: root_url,
       flash: {error: 'Database is in readonly mode'}
   end
-
-  before_action do
-    scheme = (request.env['HTTP_X_FORWARDED_PROTO'] || request.env["rack.url_scheme"]).split(',').last
-    host = request.env['HTTP_X_FORWARDED_HOST'] || request.env["HTTP_HOST"]
-    scope = request.env['RAILS_APP_SCOPE']
-    root = request.env['RAILS_RELATIVE_URL_ROOT']
-    @websocket = "#{scheme.sub('http', 'ws')}://#{host}#{root}/#{[scope, 'cable'].compact.join('/')}"
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
 
     link_to name, path_options, html_options, &block
   end
+
+  def action_cable_meta_tag_dynamic
+    scheme = (request.env['HTTP_X_FORWARDED_PROTO'] || request.env["rack.url_scheme"]).split(',').last
+    host = request.env['HTTP_X_FORWARDED_HOST'] || request.env["HTTP_HOST"]
+    scope = request.env['RAILS_APP_SCOPE']
+    root = request.env['RAILS_RELATIVE_URL_ROOT']
+    websocket = "#{scheme.sub('http', 'ws')}://#{host}#{root}/#{[scope, 'cable'].compact.join('/')}"
+    "<meta name=\"action-cable-url\" content=\"#{websocket}\" />".html_safe
+  end
 end
