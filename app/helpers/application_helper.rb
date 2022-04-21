8b2c62f0ca5d9185d7e0219473d97023a8d9e9a6
Sam Ruby <rubys@intertwingly.net>
2022-04-21 18:11:32 -0400

    fix typo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def up_link(name, path_options, html_options = {}, &block)
     if ApplicationRecord.readonly?
-      html_optinos = html_options.merge(disabled: true)
+      html_options = html_options.merge(disabled: true)
       html_options[:class] = "#{html_options[:class]} btn-disabled"
       html_options[:title] = "database is in read-only mode"
     end
