405bd7f71e205ea263ecad822a3d5b510bb28afa
Sam Ruby <rubys@intertwingly.net>
2022-04-22 17:31:10 -0400

    reflect full formation in various lists

--- app/controllers/concerns/printable.rb
+++ app/controllers/concerns/printable.rb
@@ -2,6 +2,17 @@ module Printable
   def heat_sheets
     @people ||= Person.where(type: ['Student', 'Professional']).order(:name)
     @heats = Heat.includes(:dance, entry: [:level, :age, :lead, :follow]).all.order(:number)
+
+    @heatlist = @people.map {|person| [person, []]}.to_h
+    @heats.each do |heat|
+      @heatlist[heat.lead] << heat.id rescue nil
+      @heatlist[heat.follow] << heat.id rescue nil
+    end
+
+    Formation.includes(:person, solo: :heat).each do |formation|
+      @heatlist[formation.person] << formation.solo.heat.id rescue nil
+    end
+
     @layout = 'mx-0 px-5'
     @nologo = true
     @event = Event.last
