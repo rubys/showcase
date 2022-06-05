c221fe02761d1e29188e0968bbc09141a17026c1
Sam Ruby <rubys@intertwingly.net>
2022-06-05 12:35:55 -0400

    actually commit the before_action this time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :get_authentication
+
   rescue_from ActiveRecord::ReadOnlyRecord do
     flash[:error] = 'Database is in readonly mode'
     redirect_back fallback_location: root_url,
       flash: {error: 'Database is in readonly mode'}
   end
-end
+
+  private
+    def get_authentication
+      STDERR.puts request.headers.inspect
+      if request.headers['HTTP_AUTHORIZATION']
+        @authuser = Base64.decode64(request.headers['HTTP_AUTHORIZATION'].split(' ')[1]).split(':').first
+      else
+        @authuser = request.headers["HTTP_X_REMOTE_USER"]
+      end
+    end
+endk
