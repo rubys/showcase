902c6a59d4daf5cf31de24d0ca7a3389f7068f64
Sam Ruby <rubys@intertwingly.net>
2022-07-12 15:29:33 -0400

    try config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module AmEvent
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
