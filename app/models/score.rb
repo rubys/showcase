665cf0bd64c81c39cecea4d88e97f702807bbc63
Sam Ruby <rubys@intertwingly.net>
2022-08-04 00:20:06 -0400

    accept scores even if heats fail validation

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,5 +1,5 @@
 class Score < ApplicationRecord
   belongs_to :judge, nil, class_name: 'Person'
   belongs_to :heat
-  validates_associated :heat
+  # validates_associated :heat # Too dangerous to validate during scoring
 end
