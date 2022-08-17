5f5b3212bfef735b64652f63c38b392e90c5e499
Sam Ruby <rubys@intertwingly.net>
2022-08-17 11:12:18 -0400

    remove debug statement

--- app/models/score.rb
+++ app/models/score.rb
@@ -4,7 +4,6 @@ class Score < ApplicationRecord
   # validates_associated :heat # Too dangerous to validate during scoring
 
   after_save do |score|
-    STDERR.puts 'touched'
     score.broadcast_replace_later_to "live-scores-#{ENV['RAILS_APP_DB']}",
       partial: 'scores/last_update', target: 'last-score-update',
       locals: {timestamp: score.updated_at}
