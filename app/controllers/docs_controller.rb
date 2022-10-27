2cbc29d5734449feb290a9fd23da37445e3a8ac1
Sam Ruby <rubys@intertwingly.net>
2022-10-27 11:02:05 -0400

    allow unauthenticated access to the docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,8 +1,12 @@
 class DocsController < ApplicationController
+  skip_before_action :authenticate_user
+
   def page
     file = Rails.root.join('app/views/docs', params[:page] + '.md')
+
     if File.exist? file
-      render html: Kramdown::Document.new(IO.read(file)).to_html.html_safe, layout: 'docs'
+      render html: Kramdown::Document.new(IO.read(file)).to_html.html_safe,
+        layout: 'docs'
     else
       raise ActionController::RoutingError.new('Not Found')
     end
