64f17367542c0fa0ef7e7ab62284c56152b06d71
Sam Ruby <rubys@intertwingly.net>
2023-04-05 15:28:49 -0400

    make editing of routine categories less confusing

--- app/javascript/controllers/category_override_controller.js
+++ app/javascript/controllers/category_override_controller.js
@@ -7,12 +7,31 @@ export default class extends Controller {
   connect() {
     this.toggleTarget.addEventListener('change', this.toggleForm);
     this.toggleForm();
+
+    let category_routines = document.getElementById('category_routines');
+    if (category_routines) {
+      this.toggleIncludes(category_routines.checked);
+
+      category_routines.addEventListener('click', () => {
+        this.toggleIncludes(category_routines.checked);
+      })
+    }
   }
 
   toggleForm = () => {
     let display = this.toggleTarget.checked ? 'block' : 'none';
     for (let target of this.formTargets) {
       target.style.display = display;
+    }
+  }
+
+  toggleIncludes = routine => {
+    for (let checkbox of document.querySelectorAll('input[type=checkbox]')) {
+      if (checkbox.id.startsWith('category_include[')) {
+        checkbox.disabled = routine;
+        checkbox.parentElement.parentElement.parentElement.style.display =
+          (routine ? 'none' : 'block')
+      }
     }
   }
 }
