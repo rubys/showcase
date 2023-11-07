7f26e5992c9763675dc7e61175c85aba7c697d4b
Sam Ruby <rubys@intertwingly.net>
2023-11-07 09:51:02 -0500

    actually run the command

--- bin/apply-changes.rb
+++ bin/apply-changes.rb
@@ -9,7 +9,7 @@ primary_region = Tomlrb.parse(IO.read 'fly.toml')['primary_region']
 pending = JSON.parse(IO.read('tmp/deployed.json'))['pending'] || {}
 
 if pending['add'] and not pending['add'].empty?
-  machines = JSON.parse("#{fly} machines list --json")
+  machines = JSON.parse(`#{fly} machines list --json`)
   primary = machines.find {|machine| machine['region'] == primary_region}
 end
 
@@ -36,7 +36,7 @@ end
 exit 1 unless system "#{fly} deploy"
 
 (pending['delete'] || []).each do |region|
-  machines = JSON.parse("#{fly} machines list --json")
+  machines = JSON.parse(`#{fly} machines list --json`)
   machine = machines.find {|machine| machine['region'] == primary_region}
   exit 1 unless system "#{fly} machine destroy --force #{primary} --region #{region}"
 end
