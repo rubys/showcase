5dca145b5fd96f6dcd26c43f282181bdf865a49a
Sam Ruby <rubys@intertwingly.net>
2023-11-21 10:04:45 -0500

    automatic geocoding

---
+++ app/javascript/controllers/geocode_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="geocode"
+export default class extends Controller {
+  static targets = ["name", "latitude", "longitude", "token"]
+
+  connect() {
+    this.nameTarget.addEventListener('change', () => {
+      let name = this.nameTarget.value
+
+      if (!this.tokenTarget.value) {
+        this.tokenTarget.value = name.toLowerCase().replace(/\W+/g, '')
+      }
+
+      fetch(`https://geocode.maps.co/search?q=${encodeURIComponent(name)}`)
+        .then(response => response.json())
+        .then(locations => {
+          let location = locations[0]
+          if (!location) return 
+          this.latitudeTarget.value = location.lat
+          this.longitudeTarget.value = location.lon
+        })
+    })
+  }
+}
