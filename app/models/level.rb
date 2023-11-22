2a611884a7a8b8db7ce4c03da1040eb9f884efb3
Sam Ruby <rubys@intertwingly.net>
2023-11-22 14:06:45 -0500

    fix level foreign key

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,5 +1,5 @@
 class Level < ApplicationRecord
-  has_one :event, dependent: :nullify, class_name: 'Event', foreign_key: :solo_event_id
+  has_one :event, dependent: :nullify, class_name: 'Event', foreign_key: :solo_level_id
   
   def initials
     return '*' if id == 0
