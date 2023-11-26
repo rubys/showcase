fa632ee50594d83f9a745dfdd69d9a44dd9bd5f8
Sam Ruby <rubys@intertwingly.net>
2023-11-26 11:58:40 -0500

    add FORMAT and JAVASCRIPT as options

--- fly/applications/printer/server.ts
+++ fly/applications/printer/server.ts
@@ -9,13 +9,16 @@
 // after reseting the timeout.  This is useful for ensuring that the Chrome
 // instance is "warmed-up" prior to issuing requests.
 
-import puppeteer from 'puppeteer-core'
+import puppeteer, { PaperFormat } from 'puppeteer-core'
 
 // fetch configuration fron environment variables
 const PORT = process.env.PORT || 3000
+const FORMAT = (process.env.FORMAT || "letter") as PaperFormat
+const JAVASCRIPT = (process.env.JAVASCRIPT != "false")
 const TIMEOUT = (parseInt(process.env.TIMEOUT || '15')) * 60 * 1000 // minutes
 const HOSTNAME = process.env.HOSTNAME ||
-  (process.env.FLY_APP_NAME?.endsWith("-pdf") && `${process.env.FLY_APP_NAME.slice(0, -4)}.fly.dev`)
+  (process.env.FLY_APP_NAME?.endsWith("-pdf") &&
+    `${process.env.FLY_APP_NAME.slice(0, -4)}.fly.dev`)
 
 if (!HOSTNAME) {
   console.error("HOSTNAME is required")
@@ -73,7 +76,7 @@ const server = Bun.serve({
     // main puppeteer logic: fetch url, convert to URL, return response
     try {
       // disable javascript (optional)
-      await page.setJavaScriptEnabled(false)
+      await page.setJavaScriptEnabled(JAVASCRIPT)
 
       // copy headers (including auth, excluding host) from original request
       const headers = Object.fromEntries(request.headers)
@@ -81,11 +84,13 @@ const server = Bun.serve({
       await page.setExtraHTTPHeaders(headers)
 
       // fetch page to be printed
-      await page.goto(url.href, { waitUntil: 'networkidle0' })
+      await page.goto(url.href, {
+        waitUntil: JAVASCRIPT ? 'networkidle2' : 'load'
+      })
 
-      // convert page to pdf - in US letter and in color
+      // convert page to pdf - using preferred format and in full color
       const pdf = await page.pdf({
-        format: 'letter',
+        format: FORMAT,
         printBackground: true
       })
 
