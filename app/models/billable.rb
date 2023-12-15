bfcb525e720683b68b92dd6c385a6012ee53a3d6
Sam Ruby <rubys@intertwingly.net>
2023-12-15 17:09:47 -0500

    correct billable relations

--- app/models/billable.rb
+++ app/models/billable.rb
@@ -6,11 +6,11 @@ class Billable < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :type }
   validates :price, presence: true
 
-  has_many :person_options
   has_many :package_includes, dependent: :destroy, class_name: 'PackageInclude', foreign_key: :package_id
   has_many :option_included_by, dependent: :destroy, class_name: 'PackageInclude', foreign_key: :option_id
   has_many :default_student_package_for, dependent: :nullify, class_name: 'Studio', foreign_key: :default_student_package_id
-  has_many :default_professional_package_for, dependent: :nullify, class_name: 'Studio', foreign_key: :default_student_package_id
-  has_many :default_guest_package_for, dependent: :nullify, class_name: 'Studio', foreign_key: :default_student_package_id
-  has_many :people, class_name: 'Person', foreign_key: :package_id
+  has_many :default_professional_package_for, dependent: :nullify, class_name: 'Studio', foreign_key: :default_professional_package_id
+  has_many :default_guest_package_for, dependent: :nullify, class_name: 'Studio', foreign_key: :default_guest_package_id
+  has_many :people_package, class_name: 'Person', dependent: :nullify, foreign_key: :package_id
+  has_many :people_options, class_name: 'PersonOption', dependent: :destroy, foreign_key: :option_id
 end
