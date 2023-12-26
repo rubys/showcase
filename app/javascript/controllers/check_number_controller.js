58ab4a7ca93b5700f1401a59f1929e6ccedbdb61
Sam Ruby <rubys@intertwingly.net>
2023-12-26 09:28:46 -0500

    checkbox => input on longpress with tablets

--- app/javascript/controllers/check_number_controller.js
+++ app/javascript/controllers/check_number_controller.js
@@ -1,5 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
+let pressTimer = null;
+
 // Connects to data-controller="check-number"
 export default class extends Controller {
   connect() {
@@ -9,12 +11,24 @@ export default class extends Controller {
         checkbox.focus();  // needed for safari
       });
 
+      checkbox.addEventListener('touchstart', event => {
+        if (pressTimer) clearTimeout(pressTimer);
+        if (checkbox.disabled) return;
+        let input = this.createInput(checkbox);
+        pressTimer = setTimeout(() => {
+          input.value = checkbox.checked ? "1" : "0";
+          checkbox.replaceWith(input);
+          input.focus();
+        }, 1000)
+      });
+
+      checkbox.addEventListener('touchend', event => {
+        if (pressTimer) clearTimeout(pressTimer);
+        pressTimer = null;
+      });
+
       checkbox.addEventListener("keydown", event => {
-        let input = document.createElement("input");
-        input.setAttribute("type", "text");
-        input.setAttribute("id", checkbox.getAttribute("id"));
-        input.setAttribute("name", checkbox.getAttribute("name"));
-        input.classList.add("entry-count");
+        let input = this.createInput(checkbox);
         input.value = event.key;
         checkbox.replaceWith(input);
         input.focus();
@@ -22,4 +36,15 @@ export default class extends Controller {
       });
     }
   }
+
+  createInput(checkbox, value) {
+    let input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", checkbox.getAttribute("id"));
+    input.setAttribute("name", checkbox.getAttribute("name"));
+    input.setAttribute("autofocus", "autofocus");
+    input.autofocus = true;
+    input.classList.add("entry-count");
+    return input;
+  };
 }
