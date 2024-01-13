32d0633591a45c7caf9ce77bc1bd562c8e542088
Sam Ruby <rubys@intertwingly.net>
2024-01-13 11:58:15 -0500

    show/hide couples option on packages; add/fix docs

---
+++ app/javascript/controllers/couples_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="couples"
+export default class extends Controller {
+  connect() {
+    let billable_type = document.getElementById("billable_type")
+    let billable_couples = document.getElementById("billable_couples")
+
+    function couples_visibilty() {
+      if (billable_type.value == "Student") {
+        billable_couples.parentElement.classList.remove("hidden")
+      } else {
+        billable_couples.parentElement.classList.add("hidden")
+        billable_couples.checked = false
+      }
+    }
+
+    couples_visibilty()
+
+    billable_type.addEventListener("change", couples_visibilty);
+  }
+}
