720261c98ce0097d7d66dc8970682836435db9d9
Sam Ruby <rubys@intertwingly.net>
2024-01-17 21:03:31 -0500

    allow access to invoices

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -4,7 +4,7 @@ class StudiosController < ApplicationController
 
   before_action :set_studio, only: %i[ show edit update unpair destroy heats scores invoice student_invoices send_invoice solos ]
 
-  skip_before_action :authenticate_user, only: :solos
+  skip_before_action :authenticate_user, only: %i[ index solos invoice ]
   before_action only: :solos do
     authenticate_user_or_studio(@studio.name)
   end
