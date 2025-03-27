7a20d9b42850fa1c6ebfe244c3fe34708e421d6e
Sam Ruby <rubys@intertwingly.net>
2025-03-27 09:23:14 -0400

    correctly disable judge selection boxes

--- app/javascript/controllers/auto_submit_controller.js
+++ app/javascript/controllers/auto_submit_controller.js
@@ -4,9 +4,7 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   connect() {
     for (let input of this.element.querySelectorAll("input,select")) {
-      console.log(input);
       input.addEventListener("change", _event => {
-        console.log(this.element);
         this.element.requestSubmit();
       });
     }
