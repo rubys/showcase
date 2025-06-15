fedcf6c1d8e585728783538c70db40caf08b950c
Sam Ruby <rubys@intertwingly.net>
2025-06-15 19:38:37 -0400

    active storage cleanup

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -2,4 +2,6 @@ class Recording < ApplicationRecord
   belongs_to :judge
   belongs_to :heat
   has_one_attached :audio
+
+  after_save :upload_blobs, if: -> { audio.attached? && audio.blob.created_at > 1.minute.ago }
 end
