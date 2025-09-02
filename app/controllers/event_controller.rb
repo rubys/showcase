29ea3694d285a28fb45529756168fe52bd532543
Sam Ruby <rubys@intertwingly.net>
2025-09-01 20:45:28 -0400

    revert to map.yml now that it has been updated

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -527,7 +527,7 @@ class EventController < ApplicationController
 
     @showcases.select! {|year, sites| !sites.empty?}
     if @showcases.empty?
-      @map = RegionConfiguration.generate_map_data
+      @map = YAML.load_file('config/tenant/map.yml')
       unless @map['studios'] && @map['studios'][@city]
         raise ActiveRecord::RecordNotFound
       end
