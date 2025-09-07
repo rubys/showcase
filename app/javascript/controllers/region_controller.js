fe78e90c0865a2e7a221f14551dbabe727231895
Sam Ruby <rubys@intertwingly.net>
2025-09-07 19:50:17 -0400

    machine based routing with nav

--- app/javascript/controllers/region_controller.js
+++ app/javascript/controllers/region_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="region"
 export default class extends Controller {
   connect() {
+    this.machine = this.element.dataset.machine;
     this.region = this.element.dataset.region;
 
     document.documentElement.addEventListener(
@@ -19,12 +20,18 @@ export default class extends Controller {
   }
 
   beforeFetchRequest = event => {
-    event.detail.fetchOptions.headers['Fly-Prefer-Region'] = this.region;
+    if (this.machine) {
+      event.detail.fetchOptions.headers['Fly-Prefer-Instance-Id'] = this.machine;
+    } else if (this.region) {
+      event.detail.fetchOptions.headers['Fly-Prefer-Region'] = this.region;
+    }
   }
 }
 
 window.inject_region = function(headers) {
-  if (document.body.dataset.region) {
+  if (document.body.dataset.machine) {
+    return Object.assign({}, headers, {'Fly-Prefer-Instance-Id': document.body.dataset.machine})
+  } else if (document.body.dataset.region) {
     return Object.assign({}, headers, {'Fly-Prefer-Region': document.body.dataset.region})
   } else {
     return headers
